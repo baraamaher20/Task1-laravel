eb5562df9fa7dd38ddde8fa2afddd112408e2d5f
Baraa Aldeeb <love.b2018aldeeb@gmail.com>
2023-07-18 21:04:30 +0300

    blog resources

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PersonalRequest;
+use App\Mail\ContactUsMail;
+use App\Mail\TestMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class FormController extends Controller
+{
+    function form1() {
+        return view('forms.form1');
+    }
+
+    function form1_data(Request $request) {
+        // dd( $request->all() );
+        // dd( $request->except('_token') );
+        // dd( $request->only('_token', 'name') );
+
+        // $name = $request->input('name');
+        // $age = $request->input('age');
+        $accept = $request->input('accept', 'No');
+        $name = $request->name;
+        $age = $request->age;
+
+        return "Welcome $name, your age is $age, Accept: $accept";
+    }
+
+    function form2() {
+        return view('forms.form2');
+    }
+
+    function form2_data(PersonalRequest $request) {
+
+        // 1. Request Validation
+        // $request->validate([
+        //     'name' => 'required|min:3|max:20',
+        //     'email' => 'required|ends_with:@gmail.com'
+        //     'email' => 'required|ends_with:@gmail.com'
+        //     'email' => 'required|ends_with:@gmail.com'
+        //     'email' => 'required|ends_with:@gmail.com'
+        //     'email' => 'required|ends_with:@gmail.com'
+        //     'email' => 'required|ends_with:@gmail.com'
+        // ]);
+
+        // 2. Validator Class
+        // $validator = Validator::make($request->all(), [
+        //     'name' => 'required|min:3|max:20',
+        //     'email' => 'required|ends_with:@gmail.com'
+        // ]);
+
+        // if($validator->fails()) {
+        //     return [
+        //         'data' => 'There is an error'
+        //     ];
+        // }
+
+        // 3. File Request
+
+
+        // dd($request->all());
+        $name = $request->name;
+        $email = $request->email;
+
+        return view('forms.form2_data', compact('name', 'email'));
+    }
+
+    function form3() {
+
+        return view('forms.form3');
+    }
+
+    function form3_data(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            // 'email' => 'required',
+            // 'age' => 'required',
+            // 'gender' => 'required',
+            // 'education' => 'required',
+        ]);
+
+        dd($request->all());
+    }
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('blog')->name('blog.')->group(function() {
+    Route::get('/', [BlogController::class, 'index'])->name('index');
+    Route::get('/about', [BlogController::class, 'about'])->name('about');
+    Route::get('/contact', [BlogController::class, 'contact'])->name('contact');
+    Route::get('/post', [BlogController::class, 'post'])->name('post');
+});
+
+Route::get('form1', [FormController::class, 'form1'])->name('form1');
+Route::post('form1', [FormController::class, 'form1_data'])->name('form1_data');
+
+
+Route::get('/form2', [FormController::class, 'form2'])->name('form2');
+Route::post('/form2', [FormController::class, 'form2_data'])->name('form2_data');
+
